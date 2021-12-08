40cedc1c7a6eba5522ffef23dbd1de5e6720e0de
github4maninder <maninder.email@gmail.com>
2021-12-08 15:51:51 -0500

    update the days of week

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,25 @@
 import React from "react";
 
 import "components/Application.scss";
+import DayList from "./DayList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
   return (
@@ -13,7 +32,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={"Monday"}
+            setDay={(day) => console.log(day)}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
