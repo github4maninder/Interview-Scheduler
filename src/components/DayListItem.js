f13be30fc5a391b196e7c8cfdcbe63ecb224d6d2
github4maninder <maninder.email@gmail.com>
2021-12-07 23:56:07 -0500

    We now have a DayListItem component that looks great in all scenarios

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,19 @@
 import React from "react";
+//Import the scss style
+import "components/DayListItem.scss";
+//Import the classnames library
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   );
 }
