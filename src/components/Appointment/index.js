5ed9bd8358cd6de172fca0ae938c88103a303cfd
github4maninder <maninder.email@gmail.com>
2021-12-10 18:53:30 -0500

    update index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -11,7 +11,10 @@ export default function Appointment(props) {
       {props.interview ? (
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer}
+
+
+          interviewer={props.interview.interviewer
+          }
         />
       ) : (
         <Empty />
