354846c2d3fbd48b7819847f7ecdecd4b078d0e0
github4maninder <maninder.email@gmail.com>
2021-12-11 22:55:00 -0500

    develop transition functionality

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,10 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
-} 
+  const transition = (newMode, ) => {
+    setMode(newMode);
+
+  }
+
+  return { mode, transition };
+}
