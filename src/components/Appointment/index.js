0bdbe638e44daf12ad1b0e4ee5af7b0ade4148dc
github4maninder <maninder.email@gmail.com>
2021-12-11 23:21:45 -0500

    conditionally render first components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,32 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import { moduleExpression } from "@babel/types";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
       )}
+
+      {/* {props.interview ? 
+      <Show 
+      student={props.interview.student}
+      interviewer={props.interview.interviewer} /> : <Empty />} */}
     </article>
   );
 }
