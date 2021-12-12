1c3ab39ea66f5695e03057cffa707c98c8035a27
github4maninder <maninder.email@gmail.com>
2021-12-11 23:11:03 -0500

    transition back to previous mode and set back limit

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,11 +2,21 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   const transition = (newMode, ) => {
     setMode(newMode);
 
   }
 
-  return { mode, transition };
+  // -- transition back to previous mode
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1])
+    }
+  
+  }
+
+  return { mode, transition, back };
 }
