319f3017dcd12714e08436869ac204c35ed2f9f1
github4maninder <maninder.email@gmail.com>
2021-12-11 21:49:45 -0500

    implement promise.all to access days and appointments from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,45 +3,7 @@ import axios from "axios";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "./Appointment";
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       },
-//     },
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   },
-// ];
+import { getAppointmentsForDay} from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -51,15 +13,27 @@ export default function Application(props) {
     appointments: {},
   });
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => setState((prev) => ({ ...prev, days }));
+  //const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      setDays(response.data);
-    });
+    // axios.get("/api/days").then((response) => {
+    //   setDays(response.data);
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState(prev => ({...prev, 
+        days: all[0].data, 
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }))
+      console.log(all)
+      // setDays(response.data)
+    })
   }, []);
 
   return (
