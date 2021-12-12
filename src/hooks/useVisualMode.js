ec857e611dd7716cfb8d1db40bbf27f7573d459b
github4maninder <maninder.email@gmail.com>
2021-12-11 23:17:26 -0500

    apply transition func to advance to any other node, work with replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,19 +4,20 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode, ) => {
+  // -- advance to any other mode, replacing current mode in history with a new one
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => [...prev, newMode]);
+    }
     setMode(newMode);
-
-  }
+  };
 
   // -- transition back to previous mode
   const back = () => {
     if (history.length > 1) {
       history.pop();
-      setMode(history[history.length - 1])
+      setMode(history[history.length - 1]);
     }
-  
-  }
-
+  };
   return { mode, transition, back };
 }
