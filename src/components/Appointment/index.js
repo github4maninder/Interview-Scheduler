b2355e42ce717d33eea50d2b03aaa23da6d9de70
github4maninder <maninder.email@gmail.com>
2021-12-14 10:35:43 -0500

    add confirmation dialog when deleting appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,12 +7,14 @@ import useVisualMode from "hooks/useVisualMode";
 import { moduleExpression } from "@babel/types";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 // Appointment Info
 export default function Appointment(props) {
@@ -47,7 +49,7 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={deleteFunc}
+          onDelete={() => transition(CONFIRM)}
         />
       )}
       {mode === CREATE && (
@@ -60,6 +62,13 @@ export default function Appointment(props) {
       )}
       {mode === SAVING && <Status message={"Saving"} />}
       {mode === DELETING && <Status message={"Deleting"} />}
+      {mode === CONFIRM && (
+        <Confirm
+          onCancel={() => back()}
+          message={"Are you sure you want to delete?"}
+          onConfirm={deleteFunc}
+        />
+      )}
     </article>
   );
 }
