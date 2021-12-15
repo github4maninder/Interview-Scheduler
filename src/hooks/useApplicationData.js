964859a23a344129943c7e727e9fbb474fce79e1
github4maninder <maninder.email@gmail.com>
2021-12-14 22:00:22 -0500

    update spots dynamically

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,6 +10,15 @@ export default function useApplicationData() {
     interviewers: {},
   });
 
+  const fetchFreeSpots = (appointments) => {
+    const appIds = state.days.filter((day) => day.name === state.day);
+    const todayApp = appIds[0].appointments;
+    const emptyApp = todayApp.filter(
+      (app) => !appointments[app].interview
+    ).length;
+    return emptyApp;
+  };
+
   // -- makes HTTP request; updating the state object starting at lowest level
   function bookInterview(id, interview) {
     console.log(id, interview);
@@ -21,8 +30,14 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
+    const days = [...state.days];
+    const dayIndex = state.days.findIndex((day) =>
+      day.appointments.includes(id)
+    );
+    days[dayIndex].spots = fetchFreeSpots(appointments);
+
     return axios.put(`/api/appointments/${id}`, appointment).then(() => {
-      setState({ ...state, appointments });
+      setState((prev) => ({ ...prev, appointments, days }));
     });
   }
 
@@ -36,8 +51,15 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
+
+    const days = [...state.days];
+    const dayIndex = state.days.findIndex((day) =>
+      day.appointments.includes(id)
+    );
+    days[dayIndex].spots = fetchFreeSpots(appointments);
+
     return axios.delete(`/api/appointments/${id}`).then(() => {
-      setState({ ...state, appointments });
+      setState((prev) => ({ ...prev, appointments, days }));
     });
   }
 
