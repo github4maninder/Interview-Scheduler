9ddeefafa26d6195b008f7db51c1e51ae2cab534
github4maninder <maninder.email@gmail.com>
2021-12-15 22:51:05 -0500

    test for entering name into input field

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,8 +2,7 @@ describe("Appointments", () => {
   it("should book an interview", () => {
     cy.visit("/");
     cy.contains("Monday");
-    cy.get("[alt=Add]")
-      .first()
-      .click();
+    cy.get("[alt=Add]").first().click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
   });
 });
