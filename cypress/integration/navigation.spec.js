76adf1a5aeb41ae3bb60e427cdb7ca7167b92e0f
github4maninder <maninder.email@gmail.com>
2021-12-15 22:44:57 -0500

    update test to include data-* attribute for best practices

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,8 +4,8 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-
-
-    cy.get("li").contains("Tuesday").click();
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -23,6 +23,7 @@ export default function DayListItem(props) {
       className={dayClass}
       onClick={() => props.setDay(props.name)}
       selected={props.selected}
+      data-testid="day"
     >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
