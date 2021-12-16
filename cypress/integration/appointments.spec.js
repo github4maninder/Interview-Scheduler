274d1d5ab3bb5834cc70d2f939e414271aef96ed
github4maninder <maninder.email@gmail.com>
2021-12-15 22:54:30 -0500

    test for clicking the save button

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -5,5 +5,6 @@ describe("Appointments", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
   });
 });
